6a37d18cfe1471a9791ce4ad1e8b093fffad5971
Ahmad535353 <ahmad@DESKTOP-1OABJVR.localdomain>
2022-12-27 17:43:16 -0700

    Third person shooter - Added movement and characters

---
+++ Source/TPShooter/TPShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSHOOTER_API ATPShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
